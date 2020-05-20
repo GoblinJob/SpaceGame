052cd6440d7105d04a6d282a29d25f63e6689cc7
Dmitriy <dencraft29@gmail.com>
2020-05-21 00:49:52 +0300

    Collision2

--- Chleking/Collisions/ICollisinable.cs
+++ Chleking/Collisions/ICollisinable.cs
@@ -1,19 +1,14 @@
-﻿using System;
+﻿using OpenTK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenTK;
 
-namespace Chleking
+namespace SpaceGame.Collisions
 {
     interface ICollisinable
     {
-        Vector3[] vertices { get; }
-    }
-
-    class CollisionSystem
-    {
-        
+        Vector3[] WorldVerticesCoordinate { get; }
     }
 }
