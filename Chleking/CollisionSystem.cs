30cb46fceb613329ca72d83b87ebb449be639898
Nikita@ <kokin.ru@gmail.com>
2020-05-21 00:42:22 +0300

    Collisions

---
+++ Chleking/CollisionSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Chleking
+{
+    interface ICollisinable
+    {
+        Vector3[] vertices { get; }
+    }
+
+    class CollisionSystem
+    {
+        
+    }
+}
